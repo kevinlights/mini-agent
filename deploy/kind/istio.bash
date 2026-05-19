63694373076d5b2f6d65596e8d8076a2c666bc3a
kevin <kevinlights2021@163.com>
2026-05-19 10:27:30 +0800

    add sample bookinfo app

--- deploy/kind/istio.bash
+++ deploy/kind/istio.bash
@@ -35,3 +35,26 @@ helm show values istio/ztunnel | grep -B 30 registry.istio.io
 helm install ztunnel istio/ztunnel -n istio-system --set hub=docker.m.daocloud.io/istio --wait
 
 helm ls -n istio-system
+
+mkdir -p bookinfo
+wget https://raw.githubusercontent.com/istio/istio/release-1.30/samples/bookinfo/platform/kube/bookinfo.yaml -O bookinfo/bookinfo.yaml
+kubectl apply -f bookinfo/bookinfo.yaml
+
+wget https://raw.githubusercontent.com/istio/istio/release-1.30/samples/bookinfo/platform/kube/bookinfo-versions.yaml -O bookinfo/bookinfo-versions.yaml
+kubectl apply -f bookinfo/bookinfo-versions.yaml
+
+kubectl get pods
+
+wget https://raw.githubusercontent.com/istio/istio/release-1.30/samples/bookinfo/gateway-api/bookinfo-gateway.yaml -O bookinfo/bookinfo-gateway.yaml
+kubectl apply -f bookinfo/bookinfo-gateway.yaml
+
+# By default, Istio creates a LoadBalancer service for a gateway. As you will access this gateway by a tunnel, you don’t need a load balancer. Change the service type to ClusterIP by annotating the gateway:
+kubectl annotate gateway bookinfo-gateway networking.istio.io/service-type=ClusterIP --namespace=default
+Wait for the gateway to show as programmed before continuing.
+
+# Wait for the gateway to show as programmed before continuing.
+kubectl get gateway
+
+kubectl port-forward svc/bookinfo-gateway-istio 8081:80
+# http://localhost:8081/productpage
+# If you refresh the page, you should see the display of the book ratings changing as the requests are distributed across the different versions of the reviews service.
